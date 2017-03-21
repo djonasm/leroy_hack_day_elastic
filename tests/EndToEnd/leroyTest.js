523a7b5032d8949e31e74e4dd0baa859751a387a
Ravan Scafi <rscafi@gmail.com>
2017-03-21 16:26:19 -0300

    Improve test to check complexity

--- tests/EndToEnd/leroyTest.js
+++ tests/EndToEnd/leroyTest.js
@@ -1,14 +1,29 @@
 module.exports = {
-  'Let\'s buy a Drill!' : function (browser) {
+  'Let\'s buy a Drill!': function (browser) {
     browser
       .url('http://www.leroymerlin.com.br/?region=grande_sao_paulo')
       .waitForElementVisible('body', 3000)
-      .setValue('input#search', 'furadeira')
+      .setValue('#search', 'furadeira')
       .click('.btn-search')
       .assert.containsText('h1', 'Furadeiras')
-      .click('#product-list a')
-      .click('[data-button="ecommerce"]')
-      .assert.urlContains('/carrinho')
-      .end()
+      .element('css selector', '#product-list a', function (link) {
+        this.elementIdElements(
+          link.value.ELEMENT,
+          'css selector',
+          '.m-product-thumb-title',
+          function (elements) {
+            var productId = elements.value[0].ELEMENT;
+            this.elementIdText(productId, function (result) {
+              var productName = result.value;
+
+              this.elementIdClick(productId)
+                .assert.containsText('h1', productName)
+                .click('[data-button=ecommerce]')
+                .assert.urlContains('/carrinho')
+                .assert.containsText('.column-product-info a', productName)
+                .end();
+            })
+          });
+      })
   }
 };
